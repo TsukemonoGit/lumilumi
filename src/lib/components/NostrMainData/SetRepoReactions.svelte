2a7175c29c49e966ca0eab54b709b6cc657d471a
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-06-16 14:45:05 +0900

    _???

--- src/lib/components/NostrMainData/SetRepoReactions.svelte
+++ src/lib/components/NostrMainData/SetRepoReactions.svelte
@@ -1,23 +1,53 @@
 <script lang="ts">
   import { useRelaySet } from "$lib/stores/useRelaySet";
-  import type { ReqStatus, RxReqBase } from "$lib/types";
-  import { readable } from "svelte/store";
-  import type Nostr from "nostr-typedef";
-  import { type DefaultRelayConfig, type RxNostr } from "rx-nostr";
   import { useRepReactionList } from "$lib/stores/useRepReactionList";
+  import { loginUser } from "$lib/stores/stores";
+  import type { ReqStatus, RxReqBase } from "$lib/types";
+  import type Nostr from "nostr-typedef";
+  import type { RxNostr } from "rx-nostr";
+  import { readable } from "svelte/store";
+
   export let rxNostr: RxNostr | undefined = undefined;
   export let req: RxReqBase | undefined = undefined;
+  export let events: Nostr.Event<number>[];
 
-  export let filters: Nostr.Filter[] = [];
+  let filters: Nostr.Filter[] = [];
   let result: { data: any; status: any; error: any };
-  $: if (filters.length > 0) {
-    result = useRepReactionList(rxNostr, filters, req);
+
+  let lastUpdateTimestamp = Date.now() + 2000; // 初回は余裕を持たせる
+  const updateInterval = 1000; // 1秒（ミリ秒）
+
+  $: if (events) {
+    const currentTimestamp = Date.now();
+    if (currentTimestamp - lastUpdateTimestamp > updateInterval) {
+      const eValues: string[] = events.reduce((acc: string[], item) => {
+        if ([7, 6, 16].includes(item.kind)) {
+          const tag = item.tags.find((tag) => tag[0] === "e");
+          if (tag) {
+            acc.push(tag[1]);
+          }
+        } else {
+          acc.push(item.id);
+        }
+        return acc;
+      }, []);
+
+      filters = [
+        {
+          "#e": eValues,
+          authors: [$loginUser],
+          kinds: [7, 6, 16], // 明示的に種類を指定
+        },
+      ];
+      result = useRepReactionList(rxNostr, filters, req);
+      lastUpdateTimestamp = currentTimestamp;
+    }
   }
+
   $: data = result?.data;
   $: status = result?.status;
   $: error = result?.error;
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface $$Slots {
     default: { events: Nostr.Event[]; status: ReqStatus };
     loading: Record<never, never>;
@@ -27,9 +57,9 @@
 </script>
 
 {#if $error}
-  <slot name="error" error={$error} />
+  <slot name="error" {error} />
 {:else if $data}
-  <slot events={$data?.event} status={$status} />
+  <slot events={$data.event} {status} />
 {:else if $status === "loading"}
   <slot name="loading" />
 {:else}
--- src/lib/func/reactions.ts
+++ src/lib/func/reactions.ts
@@ -1,15 +1,20 @@
-import { queryClient } from "$lib/stores/stores";
+import { app, queryClient } from "$lib/stores/stores";
 import type { UseReqOpts3, ReqStatus } from "$lib/types";
 import { createQuery } from "@tanstack/svelte-query";
-import { createRxNostr, createRxForwardReq, type EventPacket } from "rx-nostr";
+import {
+  createRxNostr,
+  createRxForwardReq,
+  type EventPacket,
+  createRxBackwardReq,
+} from "rx-nostr";
 import { get, writable, derived } from "svelte/store";
 import { Observable } from "rxjs";
-const rxNostr3 = createRxNostr({ connectionStrategy: "aggressive" }); //reaction repost用
-const req3 = createRxForwardReq("reactions");
 
+const rxNostr3 = createRxNostr(); //reaction repost用
 export function set3Relays(relays: any) {
   rxNostr3.setDefaultRelays(relays);
 }
+//const req3 = createRxForwardReq();
 export function useReq3({
   rxNostr,
   operator,
@@ -18,9 +23,15 @@ export function useReq3({
   req,
   initData,
 }: UseReqOpts3<EventPacket>) {
+  console.log(filters);
+  const req3 = createRxForwardReq();
+  // const req3 = createRxBackwardReq();
+  //const req3 = createRxBackwardReq("reactions");
+  // const rxNostr3 = get(app).rxNostr;
   const _queryClient = get(queryClient); // useQueryClient();
   //console.log(filters);
   //console.log(rxNostr3.getDefaultRelays());
+
   if (!_queryClient) {
     throw Error();
   }
@@ -40,7 +51,7 @@ export function useReq3({
 
         obs.subscribe({
           next: (v: EventPacket) => {
-            //       console.log("[packet]", v);
+            console.log("[packet]", v);
             if (fulfilled) {
               const etag = v.event.tags.find((item) => item[0] === "e");
               if (v.event.kind === 7 && etag) {
