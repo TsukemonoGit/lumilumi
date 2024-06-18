dd81596b1ed163e5f5a20c1bb0f52ff5168d69ea
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-06-18 18:18:27 +0900

    proxy

---
+++ src/lib/components/Elements/ProxyTag.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { MessagesSquare, AtSign, MessageCircle } from "lucide-svelte";
+  import Link from "./Link.svelte";
+
+  export let proxyTag: string[];
+</script>
+
+<Link href={proxyTag[1]} className={"inline-flex flex-row  text-magnum-300"}>
+  <MessageCircle size={16} />{proxyTag[2]}
+</Link>
--- src/lib/components/Note/EventCard.svelte
+++ src/lib/components/Note/EventCard.svelte
@@ -22,6 +22,7 @@
   import NoteActionButtons from "./NoteActionButtuns/NoteActionButtons.svelte";
   import RepostedNote from "./RepostedNote.svelte";
   import { onDestroy, onMount } from "svelte";
+  import ProxyTag from "../Elements/ProxyTag.svelte";
 
   let currentNoteId: string | undefined = undefined;
 
@@ -89,171 +90,195 @@
   const checkContentWarning = (tags: string[][]): string[] | undefined => {
     return tags.find((item) => item[0] === "content-warning");
   };
+
+  const checkProxy = (tags: string[][]): string[] | undefined => {
+    return tags.find((item) => item[0] === "proxy");
+  };
 </script>
 
 <div class="rounded-md border overflow-hidden {noteClass()} ">
-  {#if note.kind === 1}
-    <div class={"grid grid-cols-[auto_1fr]"}>
-      <div class="p-1">
-        {#if $showImg && metadata && profile(metadata)?.picture !== undefined}
-          <UserAvatar
-            url={profile(metadata)?.picture}
-            pubkey={note.pubkey}
-            name={profile(metadata)?.name}
-            size={mini ? 20 : 40}
-          />
-        {:else}
-          <Avatar
-            size={mini ? 20 : 40}
-            name={note.pubkey}
-            variant="beam"
-            colors={splitHexColorString(note.pubkey)}
-          />
-        {/if}
-      </div>
-      <div class="p-1">
-        <div class="flex align-middle">
-          {#if metadata}
-            <div>
-              {profile(metadata)?.display_name ?? profile(metadata)?.name}<span
-                class="text-magnum-100 text-sm mt-auto mb-auto ml-1 inline-flex"
-                >@{profile(metadata)?.name}</span
-              >
-            </div>
+  {#await checkProxy(note.tags) then tag}
+    {#if note.kind === 1}
+      <div class={"grid grid-cols-[auto_1fr]"}>
+        <div class="p-1">
+          {#if $showImg && metadata && profile(metadata)?.picture !== undefined}
+            <UserAvatar
+              url={profile(metadata)?.picture}
+              pubkey={note.pubkey}
+              name={profile(metadata)?.name}
+              size={mini ? 20 : 40}
+            />
           {:else}
-            <span class="text-magnum-100 text-sm mt-auto mb-auto break-all">
-              @{nip19.npubEncode(note.pubkey)}</span
+            <Avatar
+              size={mini ? 20 : 40}
+              name={note.pubkey}
+              variant="beam"
+              colors={splitHexColorString(note.pubkey)}
+            />
+          {/if}
+        </div>
+        <div class="p-1">
+          <div class="flex align-middle">
+            {#if metadata}
+              <div>
+                {profile(metadata)?.display_name ??
+                  profile(metadata)?.name}<span
+                  class="text-magnum-100 text-sm mt-auto mb-auto ml-1 inline-flex"
+                  >@{profile(metadata)?.name}</span
+                >
+              </div>
+            {:else}
+              <span class="text-magnum-100 text-sm mt-auto mb-auto break-all">
+                @{nip19.npubEncode(note.pubkey)}</span
+              >
+            {/if}
+            <div
+              class="inline-flex ml-auto mr-1 text-magnum-100 text-xs mt-auto mb-auto"
+            >
+              {formatAbsoluteDate(note.created_at)}
+            </div>
+          </div>
+          <hr />
+          {#await replyedEvent(note.tags) then { replyID, replyUsers }}
+            {#if replyID || replyUsers.length > 0}<div class="">
+                <Reply {replyID} {replyUsers} />
+                <hr />
+              </div>
+            {/if}
+          {/await}
+          {#await checkContentWarning(note.tags) then tag}
+            <div class="relative">
+              <div class=" max-h-64 overflow-y-auto">
+                <Content text={note.content} tags={note.tags} />
+              </div>
+              {#if tag}
+                <!-- <WarningHide1 text={tag[1]} /> -->
+                <WarningHide2 text={tag[1]} />
+              {/if}
+            </div>
+          {/await}
+
+          {#if tag}
+            <div class="text-end">
+              <ProxyTag proxyTag={tag} />
+            </div>
+          {/if}
+
+          <NoteActionButtons {note} {metadata} />
+        </div>
+      </div>
+    {:else if note.kind === 6 || note.kind === 16}
+      <!--リポスト-->
+      <div class="flex gap-1">
+        <Repeat2
+          size="20"
+          class="min-w-[20px] mt-auto mb-auto stroke-magnum-500"
+        />
+        <div class="self-center">
+          {#if $showImg && metadata && profile(metadata)?.picture !== undefined}
+            <UserAvatar
+              url={profile(metadata)?.picture}
+              name={profile(metadata)?.name}
+              pubkey={note.pubkey}
+              size={20}
+            />
+          {:else}
+            <Avatar
+              size={20}
+              name={note.pubkey}
+              variant="beam"
+              colors={splitHexColorString(note.pubkey)}
+            />
+          {/if}
+        </div>
+        <div
+          class=" mt-auto inline-block break-all break-words whitespace-pre-line"
+        >
+          {#if metadata}
+            {profile(metadata)?.display_name ?? profile(metadata)?.name}<span
+              class="text-magnum-100 text-sm">@{profile(metadata)?.name}</span
+            >
+          {:else}
+            <span class="text-magnum-100 text-sm"
+              >@{nip19.npubEncode(note.pubkey)}</span
             >
           {/if}
-          <div
-            class="inline-flex ml-auto mr-1 text-magnum-100 text-xs mt-auto mb-auto"
-          >
-            {formatAbsoluteDate(note.created_at)}
-          </div>
         </div>
-        <hr />
-        {#await replyedEvent(note.tags) then { replyID, replyUsers }}
-          {#if replyID || replyUsers.length > 0}<div class="">
-              <Reply {replyID} {replyUsers} />
-              <hr />
-            </div>
-          {/if}
-        {/await}
-        {#await checkContentWarning(note.tags) then tag}
-          <div class="relative">
-            <div class=" max-h-64 overflow-y-auto">
-              <Content text={note.content} tags={note.tags} />
-            </div>
-            {#if tag}
-              <!-- <WarningHide1 text={tag[1]} /> -->
-              <WarningHide2 text={tag[1]} />
-            {/if}
+        {#if tag}
+          <div class="text-end">
+            <ProxyTag proxyTag={tag} />
           </div>
-        {/await}
-        <NoteActionButtons {note} {metadata} />
-      </div>
-    </div>
-  {:else if note.kind === 6 || note.kind === 16}
-    <!--リポスト-->
-    <div class="flex gap-1">
-      <Repeat2
-        size="20"
-        class="min-w-[20px] mt-auto mb-auto stroke-magnum-500"
-      />
-      <div class="self-center">
-        {#if $showImg && metadata && profile(metadata)?.picture !== undefined}
-          <UserAvatar
-            url={profile(metadata)?.picture}
-            name={profile(metadata)?.name}
-            pubkey={note.pubkey}
-            size={20}
-          />
-        {:else}
-          <Avatar
-            size={20}
-            name={note.pubkey}
-            variant="beam"
-            colors={splitHexColorString(note.pubkey)}
-          />
         {/if}
+        <div class="ml-auto mr-2">
+          <NoteActionButtons {note} {metadata} />
+        </div>
       </div>
-      <div
-        class=" mt-auto inline-block break-all break-words whitespace-pre-line"
-      >
-        {#if metadata}
-          {profile(metadata)?.display_name ?? profile(metadata)?.name}<span
-            class="text-magnum-100 text-sm">@{profile(metadata)?.name}</span
-          >
-        {:else}
-          <span class="text-magnum-100 text-sm"
-            >@{nip19.npubEncode(note.pubkey)}</span
-          >
-        {/if}
-      </div>
-      <div class="ml-auto mr-2">
-        <NoteActionButtons {note} {metadata} />
-      </div>
-    </div>
 
-    {#await repostedId(note.tags) then { kind, tag }}
-      {#if tag}
-        <RepostedNote {tag} {kind} />
-      {/if}
-    {/await}
-  {:else if note.kind === 7}
-    <!--リアクション-->
-    <div class="flex gap-1">
-      <div class="w-fit"><Reaction event={note} /></div>
-      <div class="self-center">
-        {#if $showImg && metadata && profile(metadata)?.picture !== undefined}
-          <UserAvatar
-            url={profile(metadata)?.picture}
-            name={profile(metadata)?.name}
-            pubkey={note.pubkey}
-            size={20}
-          />
-        {:else}
-          <Avatar
-            size={20}
-            name={note.pubkey}
-            variant="beam"
-            colors={splitHexColorString(note.pubkey)}
-          />
+      {#await repostedId(note.tags) then { kind, tag }}
+        {#if tag}
+          <RepostedNote {tag} {kind} />
         {/if}
-      </div>
-      <div class="break-all break-words whitespace-pre-line mt-auto mb-auto">
-        {#if metadata}
-          {profile(metadata)?.display_name ?? profile(metadata)?.name}<span
-            class="text-magnum-100 text-sm mt-auto"
-            >@{profile(metadata)?.name}</span
-          >
-        {:else}
-          <span class="text-magnum-100 text-sm"
-            >@{nip19.npubEncode(note.pubkey)}</span
-          >
+      {/await}
+    {:else if note.kind === 7}
+      <!--リアクション-->
+      <div class="flex gap-1">
+        <div class="w-fit"><Reaction event={note} /></div>
+        <div class="self-center">
+          {#if $showImg && metadata && profile(metadata)?.picture !== undefined}
+            <UserAvatar
+              url={profile(metadata)?.picture}
+              name={profile(metadata)?.name}
+              pubkey={note.pubkey}
+              size={20}
+            />
+          {:else}
+            <Avatar
+              size={20}
+              name={note.pubkey}
+              variant="beam"
+              colors={splitHexColorString(note.pubkey)}
+            />
+          {/if}
+        </div>
+        <div class="break-all break-words whitespace-pre-line mt-auto mb-auto">
+          {#if metadata}
+            {profile(metadata)?.display_name ?? profile(metadata)?.name}<span
+              class="text-magnum-100 text-sm mt-auto"
+              >@{profile(metadata)?.name}</span
+            >
+          {:else}
+            <span class="text-magnum-100 text-sm"
+              >@{nip19.npubEncode(note.pubkey)}</span
+            >
+          {/if}
+        </div>
+        {#if tag}
+          <div class="text-end">
+            <ProxyTag proxyTag={tag} />
+          </div>
         {/if}
+        <div class="ml-auto">
+          <NoteActionButtons {note} {metadata} />
+        </div>
       </div>
-      <div class="ml-auto">
-        <NoteActionButtons {note} {metadata} />
-      </div>
-    </div>
 
-    {#await repostedId(note.tags) then { kind, tag }}
-      {#if tag}
-        <RepostedNote {tag} {kind} />
-      {/if}
-    {/await}
-  {:else}
-    <!--その他-->
-    <div class="break-all overflow-x-hidden">
-      kind:{note.kind}{#if metadata}
-        {profile(metadata)?.name}
-      {/if}
-    </div>
-    <hr />
-    {note.tags}
-    <hr />
-    <Content text={note.content} tags={note.tags} />
-    <NoteActionButtons {note} {metadata} />
-  {/if}
+      {#await repostedId(note.tags) then { kind, tag }}
+        {#if tag}
+          <RepostedNote {tag} {kind} />
+        {/if}
+      {/await}
+    {:else}
+      <!--その他-->
+      <div class="break-all overflow-x-hidden">
+        kind:{note.kind}{#if metadata}
+          {profile(metadata)?.name}
+        {/if}
+      </div>
+      <hr />
+      {note.tags}
+      <hr />
+      <Content text={note.content} tags={note.tags} />
+      <NoteActionButtons {note} {metadata} />
+    {/if}
+  {/await}
 </div>
