017697d5b5fe0989272db2b8fee1ce30a8d814c2
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-07-20 18:22:18 +0900

    search param tuika to hashtag ranmo tuika

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -3,54 +3,105 @@
   import { createCollapsible, melt } from "@melt-ui/svelte";
   import { X, ChevronsUpDown } from "lucide-svelte";
   import { slide } from "svelte/transition";
-  import type { Filter } from "nostr-typedef";
   import { getFollowingList } from "$lib/func/nostr";
   import { nip50relays, npubRegex } from "$lib/func/util";
   import { nip19 } from "nostr-tools";
   import SearchResult from "./SearchResult.svelte";
-  import { SvelteComponent } from "svelte";
   import { afterNavigate, beforeNavigate } from "$app/navigation";
   import { page } from "$app/stores";
+  import { get } from "svelte/store";
   import * as Nostr from "nostr-typedef";
+  import { nowProgress } from "$lib/stores/stores";
+  import type { SvelteComponent } from "svelte";
 
-  let searchWord: string = "";
-  let searchKind: number = 1;
-  let searchPubkey: string = "";
+  let searchWord = "";
+  let searchKind = 1;
+  let searchPubkey = "";
   let searchSince: number | undefined;
   let searchUntil: number | undefined;
-  let followee: boolean = false;
-  let filter: Filter;
-  let showFilter: Filter;
+  let searchHashtag: string | undefined;
+  let followee = false;
+  let filter: Nostr.Filter;
+  let showFilter: Nostr.Filter;
 
   let compRef: SvelteComponent;
-  let openSearchResult: boolean = false;
+  let openSearchResult = false;
+
+  function updateQueryParams() {
+    const params = new URLSearchParams(window.location.search);
+    searchHashtag ? params.set("t", searchHashtag) : params.delete("t");
+    searchWord ? params.set("q", searchWord) : params.delete("q");
+    searchKind !== undefined
+      ? params.set("k", String(searchKind))
+      : params.delete("k");
+    searchPubkey ? params.set("p", searchPubkey) : params.delete("p");
+    searchSince ? params.set("s", String(searchSince)) : params.delete("s");
+    searchUntil ? params.set("u", String(searchUntil)) : params.delete("u");
+    followee ? params.set("f", String(followee)) : params.delete("f");
+
+    const newUrl = `${window.location.pathname}?${params.toString()}`;
+    history.pushState({}, "", newUrl);
+  }
 
   afterNavigate(async () => {
     openSearchResult = false;
-    const params = $page.url.searchParams;
-    const hashtag = params.get("t");
-    console.log(hashtag);
-    if (hashtag) {
-      filter = {
-        kinds: [1],
-        "#t": [hashtag],
-      };
+    const params = get(page).url.searchParams;
+    searchHashtag = params.get("t") ?? undefined;
+    searchWord = params.get("q") ?? "";
+    searchKind = params.get("k") !== null ? Number(params.get("k")) : 1;
+    searchPubkey = params.get("p") ?? "";
+    searchSince = params.get("s") ? Number(params.get("s")) : undefined;
+    searchUntil = params.get("u") ? Number(params.get("u")) : undefined;
+    followee = params.get("f") === "true";
 
+    if (
+      searchKind ||
+      searchHashtag ||
+      searchWord ||
+      searchPubkey ||
+      searchSince ||
+      searchUntil
+    ) {
+      createFilter();
+      showFilter = { ...filter, limit: 50 };
       handleClickSearch();
     }
   });
 
   beforeNavigate(() => {
-    console.log("destroy");
     openSearchResult = false;
-    // compRef?.$destroy();
   });
 
   $: followingList = getFollowingList();
-  $: {
+  $: if (
+    searchKind === searchKind ||
+    searchHashtag ||
+    searchWord ||
+    searchPubkey ||
+    searchSince ||
+    searchUntil
+  ) {
+    createFilter();
+  }
+
+  const {
+    elements: { root, content, trigger },
+    states: { open },
+  } = createCollapsible({ forceVisible: true });
+
+  function getHex(str: string): string {
+    try {
+      return nip19.decode(str).data as string;
+    } catch (error) {
+      console.log("pubkey error");
+      return "";
+    }
+  }
+
+  function createFilter() {
     filter = {
-      search: searchWord !== "" ? searchWord : undefined,
-      kinds: searchKind !== null ? [searchKind] : [1],
+      search: searchWord || undefined,
+      kinds: [searchKind],
       authors: npubRegex.test(searchPubkey)
         ? [getHex(searchPubkey)]
         : followee
@@ -58,51 +109,32 @@
           : undefined,
       since: !Number.isNaN(searchSince) ? searchSince : undefined,
       until: !Number.isNaN(searchUntil) ? searchUntil : undefined,
+      "#t": searchHashtag ? [searchHashtag] : [],
     };
   }
-  //nip50relays
 
-  $: console.log(searchSince);
-  $: console.log(searchUntil);
-  // $: console.log(followee);
-  const {
-    elements: { root, content, trigger },
-    states: { open },
-  } = createCollapsible({
-    forceVisible: true,
-  });
-  const getHex = (str: string): string => {
-    try {
-      return nip19.decode(searchPubkey).data as string;
-    } catch (error) {
-      console.log("pubkey error");
-      return "";
-    }
-  };
+  function handleClickSearch() {
+    $nowProgress = true;
+    updateQueryParams();
+    showFilter = { ...filter, limit: 50 };
 
-  //filterによってイベントを取得して表示する
-  //inputをちぇっくしたりする
-  const handleClickSearch = () => {
-    showFilter = { ...filter, limit: 30 };
-    console.log(showFilter);
     if (openSearchResult) {
-      compRef.$destroy();
       openSearchResult = false;
-      // 次のティックでコンポーネントを再表示
       setTimeout(() => {
         openSearchResult = true;
       }, 10);
     } else {
       openSearchResult = true;
     }
-  };
+    $nowProgress = false;
+  }
 
-  const resetValue = () => {
+  function resetValue() {
     searchSince = undefined;
     searchUntil = undefined;
-  };
+  }
 
-  const handleClickPub = async () => {
+  async function handleClickPub() {
     try {
       const pub = await (window.nostr as Nostr.Nip07.Nostr)?.getPublicKey();
       if (pub) {
@@ -111,7 +143,7 @@
     } catch (error) {
       console.log("failed to get pubkey");
     }
-  };
+  }
 </script>
 
 <svelte:head>
@@ -120,15 +152,15 @@
 </svelte:head>
 
 <section>
-  <div class=" flex gap-2">
-    <div class=" font-medium text-magnum-400">SearchRelays</div>
+  <div class="flex gap-2">
+    <div class="font-medium text-magnum-400">SearchRelays</div>
     <div class="text-sm">
       {nip50relays.join(", ")}
     </div>
   </div>
   <div class="flex flex-wrap gap-2 mb-2">
     <div class="flex flex-col items-start justify-center">
-      <div class=" font-medium text-magnum-400">keyword</div>
+      <div class="font-medium text-magnum-400">keyword</div>
       <input
         type="text"
         id="word"
@@ -151,16 +183,14 @@
     {/if}
   </div>
 
-  <div use:melt={$root} class="  relative w-full">
+  <div use:melt={$root} class="relative w-full">
     <button
       use:melt={$trigger}
       class="flex w-full items-center justify-between bg-magnum-900/50 p-1 rounded-md"
     >
-      <span class="font-semibold text-magnum-400"> Options </span>
+      <span class="font-semibold text-magnum-400">Options</span>
       <button
-        class="relative h-8 w-8 place-items-center rounded-md text-sm
-        shadow hover:opacity-75 data-[disabled]:cursor-not-allowed
-        data-[disabled]:opacity-75 bg-magnum-600"
+        class="relative h-8 w-8 place-items-center rounded-md text-sm shadow hover:opacity-75 data-[disabled]:cursor-not-allowed data-[disabled]:opacity-75 bg-magnum-600"
         aria-label="Toggle"
       >
         <div class="justify-center flex">
@@ -181,8 +211,7 @@
           class="flex gap-2 w-full flex-wrap"
         >
           <div class="flex flex-col items-start justify-center">
-            <div class=" font-medium text-magnum-400">kind</div>
-
+            <div class="font-medium text-magnum-400">kind</div>
             <input
               type="number"
               id="kind"
@@ -193,8 +222,7 @@
             />
           </div>
           <div class="flex flex-col items-start justify-center w-full">
-            <div class=" font-medium text-magnum-400">pubkey</div>
-
+            <div class="font-medium text-magnum-400">pubkey</div>
             <div
               class="grid grid-cols-[1fr_auto] mt-1.5 divide-x divide-magnum-500 rounded-md border border-magnum-600 w-full"
             >
@@ -211,13 +239,17 @@
               >
             </div>
           </div>
-          <!-- <div class="flex flex-col items-start justify-center">
-            <DatePicker
-              bind:value={searchSince}
-              title={"Since"}
-              maxValue={$searchUntil ? $searchUntil : undefined}
+          <div class="flex flex-col items-start justify-center w-full">
+            <div class="font-medium text-magnum-400">hashtag</div>
+
+            <input
+              type="text"
+              id="hashtag"
+              class="h-10 w-full px-3 py-2 rounded-md border border-magnum-600"
+              placeholder="hashtag"
+              bind:value={searchHashtag}
             />
-          </div> -->
+          </div>
           <div class="flex flex-row items-start justify-center">
             <DateRangePicker
               bind:startTimeUnix={searchSince}
@@ -226,9 +258,7 @@
             />
             <button
               on:click={resetValue}
-              class="h-8 w-8 place-items-center rounded-md text-sm
-        shadow hover:opacity-75 data-[disabled]:cursor-not-allowed
-        data-[disabled]:opacity-75 bg-magnum-600 justify-center inline-flex mt-auto mb-1 ml-1"
+              class="h-8 w-8 place-items-center rounded-md text-sm shadow hover:opacity-75 data-[disabled]:cursor-not-allowed data-[disabled]:opacity-75 bg-magnum-600 justify-center inline-flex mt-auto mb-1 ml-1"
             >
               <X class="size-5" /></button
             >
@@ -237,7 +267,7 @@
       {/if}
     </div>
   </div>
-  <div class=" w-full">
+  <div class="w-full">
     <div
       class="border border-magnum-700 rounded-md max-h-40 break-all overflow-y-auto m-1 p-1"
     >
@@ -246,7 +276,8 @@
     </div>
   </div>
   <button
-    class=" rounded-md bg-magnum-200 px-3 w-40 py-3 font-medium text-magnum-900 hover:opacity-75 active:opacity-50"
+    class="rounded-md bg-magnum-200 px-3 w-40 py-3 font-medium text-magnum-900 hover:opacity-75 active:opacity-50 disabled:opacity-25"
+    disabled={$nowProgress}
     on:click={handleClickSearch}>Search</button
   >
 </section>
