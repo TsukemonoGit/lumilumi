77dc0135d746f2ce7021fdd6540a15e25152441e
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-08-03 18:33:13 +0900

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.ofsfljldb5o"
+    "revision": "0.1ial45cvv7g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
--- src/lib/components/NostrMainData/ListUsersCard.svelte
+++ src/lib/components/NostrMainData/ListUsersCard.svelte
@@ -3,18 +3,25 @@
   import UserMenu from "../Elements/UserMenu.svelte";
   import Metadata from "./Metadata.svelte";
   export let pubkeys: string[];
+  const depth: number = 0;
 </script>
 
 <div class="flex gap-1 flex-wrap">
   {#each pubkeys as pubkey}
     <Metadata queryKey={["metadata", pubkey]} {pubkey} let:metadata>
-      <UserMenu slot="loading" {pubkey} metadata={undefined} size={24} />
+      <UserMenu
+        slot="loading"
+        {pubkey}
+        metadata={undefined}
+        size={24}
+        {depth}
+      />
 
-      <UserMenu slot="error" {pubkey} metadata={undefined} size={24} />
+      <UserMenu slot="error" {pubkey} metadata={undefined} size={24} {depth} />
 
-      <UserMenu slot="nodata" {pubkey} metadata={undefined} size={24} />
+      <UserMenu slot="nodata" {pubkey} metadata={undefined} size={24} {depth} />
 
-      <div><UserMenu {pubkey} {metadata} size={24} /></div>
+      <div><UserMenu {pubkey} {metadata} size={24} {depth} /></div>
     </Metadata>
   {/each}
 </div>
