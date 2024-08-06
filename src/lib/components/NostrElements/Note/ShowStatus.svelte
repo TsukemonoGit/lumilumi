c5b4191f354ecba0fd254d34fc968b1bcaeb4ca4
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-08-06 20:37:22 +0900

    ステータスは2行まで(その代わりリンクがないときはタイトルにコンテント)

--- src/lib/components/NostrElements/Note/ShowStatus.svelte
+++ src/lib/components/NostrElements/Note/ShowStatus.svelte
@@ -16,16 +16,28 @@
     {@const link = event.tags.find((tag) => tag[0] === "r")?.[1] ?? ""}
     <div class="flex gap-1 items-center">
       {#if link !== ""}
-        <TrendingUp class="  min-w-[16px] min-[16px] w-[16px]" /><a
+        <TrendingUp class="min-w-[16px] min-[16px] w-[16px]" /><a
           class="underline"
           target="_blank"
           rel="noopener noreferrer"
           title={link}
           href={link}
-          >{event.content.trim() !== "" ? event.content.trim() : "link"}</a
+          ><div
+            class="truncate line-clamp-2 max-w-full"
+            style="	white-space: pre-wrap; word-break: break-word;"
+          >
+            {event.content.trim() !== "" ? event.content.trim() : "link"}
+          </div></a
         >
       {:else if event.content.trim() !== ""}
-        <TrendingUp class=" min-w-[16px] h-[16px] w-[16px]" />{event.content}
+        <TrendingUp class=" min-w-[16px] h-[16px] w-[16px]" />
+        <div
+          class="truncate line-clamp-2 max-w-full"
+          title={event.content}
+          style="	white-space: pre-wrap; word-break: break-word;"
+        >
+          {event.content}
+        </div>
       {/if}
     </div>
   </StatusGeneral>
@@ -40,13 +52,23 @@
           target="_blank"
           rel="noopener noreferrer"
           title={link}
-          href={link}>{event.content ?? "link"}</a
+          href={link}
+          ><div
+            class="truncate line-clamp-2 max-w-full"
+            style="	white-space: pre-wrap; word-break: break-word;"
+          >
+            {event.content ?? "link"}
+          </div></a
         >
       {:else if event.content.trim() !== ""}
-        <Music
-          class=" min-w-[16px] h-[16px] w-[16px]"
-        />{event.content.trim() !== "" ? event.content.trim() : "link"}
-      {/if}
+        <Music class=" min-w-[16px] h-[16px] w-[16px]" />
+        <div
+          class="truncate line-clamp-2 max-w-full"
+          title={event.content.trim() !== "" ? event.content.trim() : "link"}
+          style="	white-space: pre-wrap; word-break: break-word;"
+        >
+          {event.content.trim() !== "" ? event.content.trim() : "link"}
+        </div>{/if}
     </div>
   </StatusMusic>
 </div>
