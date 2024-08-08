5d898e48e33c2ae2b1830271c67d38e9f8622850
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-08-08 10:42:58 +0900

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.jc71dooqt4g"
+    "revision": "0.i8qo0p6u17o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
--- src/lib/components/Elements/OgpCard.svelte
+++ src/lib/components/Elements/OgpCard.svelte
@@ -11,17 +11,16 @@
 <div
   class=" rounded-lg m-1 hover:opacity-80 z-20 break-all border border-magnum-200 overflow-hidden"
 >
-  <a class="" href={url} title={url} target="_blank" rel="noopener noreferrer">
+  <a href={url} title={url} target="_blank" rel="noopener noreferrer">
     {#if $showImg && contents.image && contents.image !== ""}
       <div class="grid grid-cols-[0.5fr_1.5fr] divide-x divide-magnum-200">
-        <div class="overflow-hidden max-h-[8rem] flex justify-center">
-          <img
-            loading="lazy"
-            class="object-contain max-h-[8rem] max-w-full"
-            src={contents.image}
-            alt=""
-          />
-        </div>
+        <img
+          loading="lazy"
+          class="object-contain max-h-[8rem] max-w-full overflow-hidden m-auto"
+          src={contents.image}
+          alt=""
+        />
+
         <div class="p-0.5 pl-1 grid grid-rows-[auto_1fr] w-full">
           <div class="line-clamp-2 text-sm font-bold text-magnum-200">
             {contents.title}
@@ -59,9 +58,13 @@
           {contents.description}
         </div>
         <div class="flex flex-row-reverse">
-          <div class="object-contain w-5 pl-0.5 pr-0.5">
-            <Avatar size={20} name={contents.favicon} variant="beam" />
-          </div>
+          <Avatar
+            class="object-contain w-5 pl-0.5 pr-0.5"
+            size={20}
+            name={contents.favicon}
+            variant="beam"
+          />
+
           <div class="text-xs text-magnum-200">
             {#if contents.memo}
               {contents.memo} /
