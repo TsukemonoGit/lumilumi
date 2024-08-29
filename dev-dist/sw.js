56efd0fad08b2890b08e78c3ac6331f8eec1673a
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-08-29 15:30:32 +0900

    通知欄にフィルター的なやつつけてみるやつ

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.dusl3erkhg"
+    "revision": "0.h1k0an7op9"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
--- src/routes/notifications/+page.svelte
+++ src/routes/notifications/+page.svelte
@@ -11,6 +11,12 @@
   import { onMount } from "svelte";
   import OpenPostWindow from "$lib/components/OpenPostWindow.svelte";
   import type { QueryKey } from "@tanstack/svelte-query";
+  import { createTabs, createToggleGroup, melt } from "@melt-ui/svelte";
+  import { crossfade } from "svelte/transition";
+  import { cubicInOut } from "svelte/easing";
+  import * as Nostr from "nostr-typedef";
+  import { Heart, Repeat2, Reply, Zap } from "lucide-svelte";
+  import NotificationList from "./NotificationList.svelte";
   let amount = 50;
   let viewIndex = 0;
   // const [tie, tieMap] = createTie();
@@ -51,6 +57,71 @@
       since = ev[0].event.created_at;
     }
   }
+
+  //---------------------------
+  // const {
+  //   elements: { root, list, content, trigger },
+  //   states: { value },
+  // } = createTabs({
+  //   defaultValue: "all",
+  // });
+
+  let className = "";
+  export { className as class };
+
+  const triggers = [
+    // { id: "all", title: "all" }, // <p> を削除し、単なる文字列として扱う
+
+    { id: "reaction", title: Heart }, // アイコンコンポーネント
+    { id: "reply", title: Reply }, // アイコンコンポーネント
+    { id: "repost", title: Repeat2 }, // アイコンコンポーネント
+    { id: "zap", title: Zap }, // Zapを追加
+    { id: "other", title: "other" },
+  ];
+  const {
+    elements: { root, item },
+    states: { value },
+  } = createToggleGroup({
+    type: "multiple",
+    defaultValue: triggers.map((trigger) => trigger.id), //初期は全部選択
+  });
+  const [send, receive] = crossfade({
+    duration: 250,
+    easing: cubicInOut,
+  });
+  // 複数の選択に対応したフィルタリングロジック
+  const getFilteredEvents = (
+    events: Nostr.Event[],
+    selectedStates: string | string[] | undefined
+  ): Nostr.Event[] => {
+    if (!selectedStates || typeof selectedStates === "string") return events;
+    return events.filter((event) => {
+      return selectedStates.some((state) => {
+        switch (state) {
+          case "reply":
+            return event.kind === 1;
+          case "reaction":
+            return event.kind === 7;
+          case "repost":
+            return event.kind === 6 || event.kind === 16;
+          case "zap":
+            return event.kind === 9735;
+          case "other":
+            return event.kind === 42;
+          default:
+            return false;
+        }
+      });
+    });
+  };
+  $: console.log($value); //= ['reply', 'reaction', 'repost']みたいに選択されてるIDのりすとになる
+
+  const handleClickAll = () => {
+    value.set(triggers.map((trigger) => trigger.id));
+  };
+  const handleClickNone = () => {
+    value.set([]);
+  };
 </script>
 
 <svelte:head>
@@ -60,6 +131,36 @@
 </svelte:head>
 <section>
   <div class="w-full break-words overflow-x-hidden">
+    <div use:melt={$root} class=" flex items-center">
+      <button
+        class="toggle-item relative w-full"
+        disabled={$value?.length === triggers.length}
+        on:click={handleClickAll}
+      >
+        All
+      </button>
+
+      {#each triggers as triggerItem}
+        <button
+          use:melt={$item(triggerItem.id)}
+          class="toggle-item relative w-full"
+        >
+          <!-- Svelteコンポーネントとしてアイコンを扱う -->
+          {#if typeof triggerItem.title === "string"}
+            {triggerItem.title}
+          {:else}
+            <svelte:component this={triggerItem.title} />
+          {/if}
+        </button>
+      {/each}
+      <button
+        class="toggle-item relative w-full"
+        disabled={$value?.length === 0}
+        on:click={handleClickNone}
+      >
+        None
+      </button>
+    </div>
     {#if since}
       <TimelineList
         queryKey={timelineQuery}
@@ -91,13 +192,7 @@
           class="max-w-[100vw] break-words box-border divide-y divide-magnum-600/30 w-full"
         >
           {#if events && events.length > 0}
-            {#each events as event, index (event.id)}
-              <!-- <div
-                class="max-w-full break-words whitespace-pre-line box-border overflow-hidden {index ===
-                events.length - 1
-                  ? 'last-visible'
-                  : ''} {index === 0 ? 'first-visible' : ''}"
-              > -->
+            {#each getFilteredEvents(events, $value) as event, index (event.id)}
               <Metadata
                 queryKey={["metadata", event.pubkey]}
                 pubkey={event.pubkey}
@@ -114,8 +209,8 @@
                 </div>
                 <EventCard {metadata} note={event} /></Metadata
               >
-              <!-- </div> -->
-            {/each}{/if}
+            {/each}
+          {/if}
         </div>
       </TimelineList>{/if}
   </div>
@@ -129,12 +224,90 @@
   />
 </div>
 
-<style>
+<style lang="postcss">
   section {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     flex: 0.6;
+  }
+
+  .toggle-item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    user-select: none;
+
+    border-radius: 0;
+    background-color: theme("colors.magnum.900");
+
+    color: theme("colors.magnum.200");
+    font-weight: 500;
+    line-height: 1;
+
+    flex: 1;
+    height: theme(spacing.12);
+    padding-inline: theme(spacing.2);
+
+    &:focus {
+      position: relative;
+    }
+
+    &:focus-visible {
+      @apply z-10 ring-2;
+    }
+
+    &[data-state="active"] {
+      @apply focus:relative;
+      background-color: theme("colors.magnum.700");
+      color: theme("colors.magnum.100");
+    }
+  }
+
+  input {
+    height: theme(spacing.8);
+    flex-shrink: 0;
+    flex-grow: 1;
+    border-radius: theme(borderRadius.md);
+    border: 1px solid theme(colors.neutral.200);
+    padding-inline: theme(spacing[2.5]);
+    line-height: 1;
+    color: theme(colors.neutral.900);
+
+    &:focus {
+      border-color: theme(colors.magnum.400);
+    }
+  }
+
+  .save {
+    display: inline-flex;
+    height: theme(spacing.8);
+    cursor: default;
+    align-items: center;
+    justify-content: center;
+    border-radius: theme(borderRadius.md);
+    background-color: theme(colors.magnum.200);
+    padding-inline: theme(spacing.4);
+    line-height: 1;
+    font-weight: theme(fontWeight.semibold);
+    color: theme(colors.magnum.900);
+    @apply transition;
+
+    &:hover {
+      opacity: 0.75;
+    }
+
+    &:focus {
+      @apply !ring-green-600;
+    }
+  }
+
+  .toggle-item[data-state="on"] {
+    @apply bg-magnum-200 text-magnum-900;
+  }
+  .toggle-item:disabled {
+    @apply bg-magnum-200 text-magnum-900;
   }
 </style>
