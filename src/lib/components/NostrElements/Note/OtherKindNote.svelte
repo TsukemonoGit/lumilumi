d4bcefa2f668c36827c69e7ede258f17b24746d4
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-09-17 08:27:19 +0900

    fix

--- src/lib/components/NostrElements/Note/OtherKindNote.svelte
+++ src/lib/components/NostrElements/Note/OtherKindNote.svelte
@@ -2,10 +2,18 @@
   import * as Nostr from "nostr-typedef";
   import Content from "./Content.svelte";
   import UserMenu from "$lib/components/Elements/UserMenu.svelte";
-  import { eventKinds, profile } from "$lib/func/util";
+  import {
+    datetime,
+    eventKinds,
+    formatAbsoluteDate,
+    profile,
+  } from "$lib/func/util";
   import { showImg } from "$lib/stores/stores";
   import NoteActionButtons from "./NoteActionButtuns/NoteActionButtons.svelte";
   import SimpleMarkdown from "$lib/components/MarkdownItem/SimpleMarkdown.svelte";
+  import { nip19 } from "nostr-tools";
+  import { getRelaysById } from "$lib/func/nostr";
+  import { goto } from "$app/navigation";
 
   export let note: Nostr.Event;
   export let metadata: Nostr.Event | undefined;
@@ -21,10 +29,36 @@
       (tag[0] === "description" || tag[0] === "summary") && tag.length > 1
   )?.[1];
   $: image = note.tags.find((tag) => tag[0] === "image" && tag.length > 1)?.[1];
+
+  const replaceable =
+    (note.kind >= 30000 && note.kind < 40000) ||
+    (note.kind >= 10000 && note.kind < 20000) ||
+    note.kind === 0 ||
+    note.kind === 3;
+  const eventpointer: nip19.EventPointer = {
+    id: note.id,
+    relays: getRelaysById(note.id),
+    author: note.pubkey,
+    kind: note.kind,
+  };
+  const naddrpointer: nip19.AddressPointer = {
+    kind: note.kind,
+    identifier: note.tags.find((item) => item[0] === "d")?.[1] ?? "",
+    pubkey: note.pubkey,
+    relays: getRelaysById(note.id),
+  };
+  const naddr = nip19.naddrEncode(naddrpointer);
+  const nevent = nip19.neventEncode(eventpointer);
+
+  const handleClickToNotepage = () => {
+    //Goto Note page
+
+    goto(`/${replaceable ? naddr : nevent}`);
+  };
 </script>
 
 <div class=" break-all overflow-x-hidden gap-4 p-1">
-  <div class="flex gap-1 w-fit">
+  <div class="w-full flex gap-1">
     {#if metadata}
       <div>
         <UserMenu
@@ -43,6 +77,16 @@
       <div class="text-neutral-300/50 text-sm">
         {eventKinds.get(note.kind)?.en ?? `kind:${note.kind}`}
       </div>
+      {#if displayMenu}
+        <button
+          on:click={handleClickToNotepage}
+          class="  ml-auto mr-1 min-w-7 text-magnum-100 text-xs hover:underline"
+        >
+          <time datetime={datetime(note.created_at)}
+            >{formatAbsoluteDate(note.created_at)}</time
+          >
+        </button>
+      {/if}
     {/if}
   </div>
   <div class="grid grid-cols-[1fr_auto] w-full gap-1">
