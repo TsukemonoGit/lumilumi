7fed4ca630628e17c93f22d5f671669bfefc7d89
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-10-29 20:12:40 +0900

    fix relay sample link

--- src/lib/components/NostrElements/Note/SampleGlobalLink.svelte
+++ src/lib/components/NostrElements/Note/SampleGlobalLink.svelte
@@ -6,20 +6,22 @@
   <div class="font-bold mt-2">sample global timeline link:</div>
   <ul>
     <li>
-      <a class="underline text-magnum-400 :opacity-75" href={`/relay/nos.lol`}
-        >{`${$page.url.origin}/relay/nos.lol`}</a
+      <a
+        class="underline text-magnum-400 :opacity-75"
+        href={`/relay/${encodeURIComponent("wss://nos.lol")}`}>wss://nos.lol</a
       >
     </li>
     <li>
-      <a class="underline text-magnum-400 :opacity-75" href={`/relay/yabu.me`}
-        >{`${$page.url.origin}/relay/yabu.me`}</a
+      <a
+        class="underline text-magnum-400 :opacity-75"
+        href={`/relay/${encodeURIComponent("wss://yabu.me")}`}>wss://yabu.me</a
       > ( japan only relay )
     </li>
     <li>
       <a
         class="underline text-magnum-400 :opacity-75"
-        href={`/relay/catstrr.swarmstr.com/`}
-        >{`${$page.url.origin}/relay/catstrr.swarmstr.com/`}</a
+        href={`/relay/${encodeURIComponent("wss://catstrr.swarmstr.com/")}`}
+        >wss://catstrr.swarmstr.com/</a
       > (cat only relay)
     </li>
   </ul>
