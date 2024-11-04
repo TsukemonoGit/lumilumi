e52e100c1a34dbefbd6653a3e9315d70aa17d23b
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-11-04 10:08:23 +0900

    content warning layout

--- src/lib/components/Elements/WarningHide2.svelte
+++ src/lib/components/Elements/WarningHide2.svelte
@@ -16,23 +16,34 @@
 </script>
 
 <div
-  class="absolute top-0 left-0 w-full h-full flex bg-neutral-700 {$value[0] >=
+  class="rounded-md border border-magnum-500/40 absolute top-0 left-0 w-full h-full bg-neutral-800 {$value[0] >=
   50
     ? 'pointer-events-none'
     : ''}"
   style=" opacity:{1 - $value[0] / 100}"
 >
+  <!-- TriangleAlertアイコン 
+ <TriangleAlert
+ size={"100%"}
+ class="relative text-magnum-500/35 min-w-4  left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-h-52"
+/>-->
+  <!-- スクロール可能なテキスト領域 -->
   <div
-    class=" grid grid-cols-[auto_1fr_auto] justify-between w-full overflow-hidden overflow-y-auto py-1"
+    class="absolute w-full top-0 text-magnum-500 font-bold text-xs flex justify-center items-center gap-1"
   >
-    <TriangleAlert size="20" class="text-magnum-500 min-w-4 mx-1" />
-    <div class="w-full text-center">{text ?? "warning"}</div>
-    <TriangleAlert size="20" class="text-magnum-500 min-w-4 mx-1" />
+    <!-- TriangleAlertアイコン -->
+    <TriangleAlert size="16" />WARNING<TriangleAlert size="16" />
+  </div>
+  <div
+    class="absolute w-full max-h-[calc(100%-24px)] overflow-y-auto text-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-magnum-100 text-sm p-1"
+  >
+    {text}
   </div>
 </div>
+
 <span
   use:melt={$root}
-  class="relative flex h-[24px] w-[95%] mx-auto items-center"
+  class="relative flex h-[24px] w-[calc(100%-24px)] mx-auto items-center"
 >
   <span class="h-[3px] w-full bg-magnum-100/40">
     <span use:melt={$range} class="h-[3px] bg-neutral-900/80" />
@@ -40,6 +51,6 @@
 
   <span
     use:melt={$thumbs[0]}
-    class="h-5 w-5 rounded-full bg-magnum-500 focus:ring-4 focus:!ring-magnum-300/40"
+    class="h-5 w-5 rounded-full bg-magnum-500 focus:ring-4 focus:!ring-magnum-400/40"
   />
 </span>
