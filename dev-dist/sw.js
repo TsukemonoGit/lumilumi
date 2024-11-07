f875bc5c8e1d2074f73fc0287f2271410f1fb0c6
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-11-08 08:51:40 +0900

    settings error fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.sdm6203rvt"
+    "revision": "0.locvgicj538"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
