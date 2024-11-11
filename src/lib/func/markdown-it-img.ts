4f7dc3298e5cd2c600d2b2e819f565d92a09adf4
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-11-11 09:56:34 +0900

    30023 no html img tag

--- src/lib/func/markdown-it-img.ts
+++ src/lib/func/markdown-it-img.ts
@@ -1,32 +1,32 @@
 import type MarkdownIt from "markdown-it";
 
+const parseImgAttributes = (imgTag: string) => {
+  const attributes: [string, string][] = [];
+  const attrRegex = /\b(alt|src|width|height|style)=["']?([^"'>]+)["']?/gi;
+  let attrMatch;
+
+  while ((attrMatch = attrRegex.exec(imgTag)) !== null) {
+    attributes.push([attrMatch[1], attrMatch[2]]);
+  }
+
+  return attributes;
+};
+
 const markdownImgPlugin = function (md: MarkdownIt) {
   md.inline.ruler.before("image", "html_img", function (state, silent) {
     const pos = state.pos;
 
-    // imgタグ全体にマッチする正規表現
-    const imgRegex = /<img\s+([^>]+?)\s*\/?>/i;
+    // <img> タグ全体にマッチする正規表現
+    const imgRegex = /<img\s+[^>]+\/?>/i;
     const match = imgRegex.exec(state.src.slice(pos));
 
     if (match) {
       if (!silent) {
         const token = state.push("image", "img", 0);
-        token.attrs = [];
-
-        // 属性名（alt, src, width, height, style）とその値をマッチさせる正規表現
-        const attrRegex =
-          /\b(alt|src|width|height|style)=["']?([^"'\s>]+)["']?/gi;
-        let attrMatch;
-
-        // 各属性を順番に取得してtoken.attrsに追加
-        while ((attrMatch = attrRegex.exec(match[1])) !== null) {
-          const attrName = attrMatch[1];
-          const attrValue = attrMatch[2];
-          token.attrs.push([attrName, attrValue]);
-        }
+        token.attrs = parseImgAttributes(match[0]);
       }
 
-      // マッチした部分の長さだけ位置を進める
+      // 正しい位置に進める
       state.pos += match[0].length;
       return true;
     }
