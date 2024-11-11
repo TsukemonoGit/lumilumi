4fb0d8d3a070987811a9d366c887c2e375c8605a
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-11-11 23:05:23 +0900

    test

--- src/routes/post/+page.server.ts
+++ src/routes/post/+page.server.ts
@@ -1,38 +1,25 @@
-import { json, error } from "@sveltejs/kit";
+import { json, error, fail } from "@sveltejs/kit";
 
 export const actions = {
   default: async ({ request }) => {
     try {
-      const formData = new URLSearchParams(await request.text());
-      console.log(formData);
-      // Extract the data from the form
+      // FormDataを使用してリクエストボディを取得
+      const formData = await request.formData();
+
+      // フォームデータから値を取得
       const title = formData.get("title");
       const text = formData.get("text");
       const url = formData.get("url");
       const media = formData.get("media");
 
-      // Log extracted data to check their values
-      console.log("Title:", title);
-      console.log("Text:", text);
-      console.log("URL:", url);
-      console.log("Media:", media);
-
       if (!title && !text && !url && !media) {
-        throw error(400, "Missing required fields");
+        return fail(400, { message: "invalid" });
       }
 
-      // Here, you can process the data, e.g., save to the database or handle it
-      // For the sake of this example, let's return the submitted data
-      return json({
-        success: true,
-        title,
-        text,
-        url,
-        media,
-      });
+      return { success: true };
     } catch (err) {
       console.error(err);
-      throw error(500, "Server error");
+      return fail(400, { message: "invalid" });
     }
   },
 };
--- src/routes/post/+page.svelte
+++ src/routes/post/+page.svelte
@@ -4,66 +4,99 @@
   import { additionalPostOptions, postWindowOpen } from "$lib/stores/stores";
   import { onMount } from "svelte";
 
-  export let data: {
-    title: string;
-    text: string;
-    url: string;
-    media: string;
-  };
+  // Web Share Target API
+  //https://developer.mozilla.org/ja/docs/Web/Manifest/share_target
+  //マニフェストからのやつ
+  // export let data: {
+  //   title: string;
+  //   text: string;
+  //   url: string;
+  //   media: string;
+  // };
 
+  let fileList: FileList;
   let sharedContent: string = "";
   let tags: string[][] = [];
-  let fileList = data.media ? createFileList(new File([], data.media)) : null;
+  // $: fileList = data.media ? createFileList(new File([], data.media)) : null;
 
-  sharedContent = [data.title, data.text, data.url]
-    .filter((param) => param !== null)
-    .join("\n");
+  // sharedContent = [data.title, data.text, data.url]
+  //   .filter((param) => param !== null)
+  //   .join("\n");
 
   onMount(async () => {
-    console.log(data);
     try {
-      let uploader = localStorage.getItem("uploader") || mediaUploader[0];
+      // Service Workerからのメッセージを受け取る
+      if (navigator?.serviceWorker) {
+        navigator.serviceWorker.addEventListener("message", (event) => {
+          const { title, text, url, files } = event.data;
 
-      if (fileList) {
-        const url = await filesUpload(fileList, uploader);
-
-        url.map(async (data) => {
-          if (data.status === "success") {
-            const url = data.nip94_event?.tags.find(
-              (tag) => tag[0] === "url"
-            )?.[1];
-
-            if (url) {
-              if (data.nip94_event) {
-                tags.push(convertMetaTags(data.nip94_event));
-              }
-              sharedContent += url;
-            }
-          }
+          // 受け取ったデータを処理
+          console.log("Received data from service worker:", {
+            title,
+            text,
+            url,
+            files,
+          });
+          fileList = files;
+          sharedContent = [title, text, url]
+            .filter((param) => param !== null)
+            .join("\n");
         });
-      }
 
-      $additionalPostOptions = {
-        tags,
-        addableUserList: [],
-        defaultUsers: [],
-        warningText: undefined,
-        content: sharedContent,
-      };
-      $postWindowOpen = true;
+        let uploader = localStorage.getItem("uploader");
+        if (!uploader) {
+          uploader = mediaUploader[0];
+        }
+
+        if (fileList) {
+          const url = await filesUpload(fileList, uploader);
+
+          url.map(async (data) => {
+            if (data.status === "success") {
+              const url = data.nip94_event?.tags.find(
+                (tag) => tag[0] === "url"
+              )?.[1];
+
+              if (url) {
+                // imetaをタグに入れる
+                if (data.nip94_event) {
+                  tags.push(convertMetaTags(data.nip94_event));
+                }
+                sharedContent = sharedContent + url;
+              }
+            }
+          });
+        }
+        $additionalPostOptions = {
+          tags: tags,
+          addableUserList: [],
+          defaultUsers: [],
+          warningText: undefined,
+          content: sharedContent,
+        };
+        $postWindowOpen = true;
+      }
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   });
 
-  function createFileList(file: File): FileList {
-    const dataTransfer = new DataTransfer();
-    dataTransfer.items.add(file);
-    return dataTransfer.files;
-  }
+  // // FileをFileListに変換する関数
+  // function createFileList(file: File): FileList {
+  //   const dataTransfer = new DataTransfer();
+  //   dataTransfer.items.add(file);
+  //   return dataTransfer.files;
+  // }
+  // // サーバーから共有データを取得する関数
+  // async function getSharedData(): Promise<FormData> {
+  //   const response = await fetch("/post", {
+  //     method: "POST",
+  //   });
+  //   const formData = await response.formData();
+  //   return formData;
+  // }
 </script>
 
-// src/routes/post/+page.svelte
 <div class="postWindow">
   <OpenPostWindow
     options={{
