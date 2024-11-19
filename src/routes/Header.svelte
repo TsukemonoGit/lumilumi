3e5ac6989673b6cedfaa438ddb0d7d4b524e398f
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-11-19 12:37:40 +0900

    最大の横幅ちょっと狭くしてみる

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -49,75 +49,83 @@
       <RelayStatus />
     </div>
     {#if $currentPage?.alt !== "settings"}<!--&& $currentPage?.alt !== "about"-->
-      <div class="absolute right-1 top-0 h-8 flex">
-        <Popover
-          bind:open={openPopover}
-          ariaLabel="timeline filter setting"
-          showCloseButton={true}
-        >
-          <div
-            class=" flex items-center font-bold text-sm gap-1 rounded-full border text-magnum-200 border-magnum-300
+      <div class="fixed w-full top-0">
+        <div class="container relative">
+          <div class="option">
+            <Popover
+              bind:open={openPopover}
+              ariaLabel="timeline filter setting"
+              showCloseButton={true}
+            >
+              <div
+                class=" flex items-center font-bold text-sm gap-1 rounded-full border text-magnum-200 border-magnum-300
                 hover:bg-magnum-800/25 active:bg-magnum-700/25 px-3 py-1"
-          >
-            <SlidersHorizontal size={16} />Options
-          </div>
-          <div slot="popoverContent" class="w-[320px] max-w-full flex flex-col">
-            <ul>
-              {#if $currentPage?.alt === "home"}
-                <li class="mb-2">
-                  <div class="label">{$_("filter.menu.canversation")}</div>
-                  <div
-                    use:melt={$root}
-                    class="text-sm my-1 gap-1 flex flex-col data-[orientation=horizontal]:flex-row"
-                    aria-label="View density"
-                  >
-                    {#each optionsArr as [index, option]}
-                      <div class="flex items-center gap-3">
-                        <button
-                          use:melt={$item(index)}
-                          class="grid h-6 w-6 cursor-default place-items-center rounded-full border border-magnum-400 shadow-sm
+              >
+                <SlidersHorizontal size={16} />Options
+              </div>
+              <div
+                slot="popoverContent"
+                class="w-[320px] max-w-full flex flex-col"
+              >
+                <ul>
+                  {#if $currentPage?.alt === "home"}
+                    <li class="mb-2">
+                      <div class="label">{$_("filter.menu.canversation")}</div>
+                      <div
+                        use:melt={$root}
+                        class="text-sm my-1 gap-1 flex flex-col data-[orientation=horizontal]:flex-row"
+                        aria-label="View density"
+                      >
+                        {#each optionsArr as [index, option]}
+                          <div class="flex items-center gap-3">
+                            <button
+                              use:melt={$item(index)}
+                              class="grid h-6 w-6 cursor-default place-items-center rounded-full border border-magnum-400 shadow-sm
       hover:bg-magnum-800"
-                          id={option}
-                          aria-labelledby="{option}-label"
-                        >
-                          {#if $isChecked(index)}
-                            <div class="h-3 w-3 rounded-full bg-magnum-400" />
-                          {/if}
-                        </button>
-                        <label for={option} id="{option}-label">
-                          {option}
-                        </label>
+                              id={option}
+                              aria-labelledby="{option}-label"
+                            >
+                              {#if $isChecked(index)}
+                                <div
+                                  class="h-3 w-3 rounded-full bg-magnum-400"
+                                />
+                              {/if}
+                            </button>
+                            <label for={option} id="{option}-label">
+                              {option}
+                            </label>
+                          </div>
+                        {/each}
+                        <input name="line-height" use:melt={$hiddenInput} />
                       </div>
-                    {/each}
-                    <input name="line-height" use:melt={$hiddenInput} />
-                  </div>
-                </li>
-              {/if}
-              <li>
-                <label class="label">
-                  <input
-                    type="checkbox"
-                    class="rounded-checkbox"
-                    bind:checked={$timelineFilter.adaptMute}
-                  />
-                  {$_("filter.menu.muteOn")}
-                </label>
-              </li>
-              <li>
-                <label class="label">
-                  <input
-                    type="checkbox"
-                    class="rounded-checkbox"
-                    bind:checked={$showBanner}
-                  />
-                  {$_("settings.display.banner")}
-                </label>
-              </li>
-            </ul>
-          </div></Popover
-        >
-      </div>
-    {/if}
+                    </li>
+                  {/if}
+                  <li>
+                    <label class="label">
+                      <input
+                        type="checkbox"
+                        class="rounded-checkbox"
+                        bind:checked={$timelineFilter.adaptMute}
+                      />
+                      {$_("filter.menu.muteOn")}
+                    </label>
+                  </li>
+                  <li>
+                    <label class="label">
+                      <input
+                        type="checkbox"
+                        class="rounded-checkbox"
+                        bind:checked={$showBanner}
+                      />
+                      {$_("settings.display.banner")}
+                    </label>
+                  </li>
+                </ul>
+              </div></Popover
+            >
+          </div>
+        </div>
+      </div>{/if}
   </div>
 </header>
 
@@ -139,5 +147,15 @@
   }
   .label {
     list-style-type: disc;
+  }
+
+  .option {
+    @apply absolute top-0 h-8 flex  right-8;
+  }
+
+  @media (max-width: 768px) {
+    .option {
+      @apply absolute top-0 h-8 flex right-0;
+    }
   }
 </style>
