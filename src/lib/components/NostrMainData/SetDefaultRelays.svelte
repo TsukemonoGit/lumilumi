60eb717e2fad818355364521e5a0e63685fef68a
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-11-30 06:56:27 +0900

    fix

--- src/lib/components/NostrMainData/SetDefaultRelays.svelte
+++ src/lib/components/NostrMainData/SetDefaultRelays.svelte
@@ -1,8 +1,7 @@
-<!-- @migration-task Error while migrating Svelte code: This migration would change the name of a slot making the component unusable -->
 <script lang="ts">
   import { useRelaySet } from "$lib/stores/useRelaySet";
-  import type { ReqStatus } from "$lib/types";
-  import { readable } from "svelte/store";
+  import type { ReqResult, ReqStatus } from "$lib/types";
+  import { readable, type Readable } from "svelte/store";
   import type Nostr from "nostr-typedef";
   import {
     type DefaultRelayConfig,
@@ -18,7 +17,7 @@
   interface Props {
     pubkey: string;
     localRelays: DefaultRelayConfig[];
-    paramRelays: string[];
+    paramRelays: string[] | undefined;
     req?:
       | (RxReq<"backward"> &
           RxReqEmittable<{
@@ -27,6 +26,7 @@
           RxReqOverable &
           RxReqPipeable)
       | undefined;
+    //relayChange: (data: string[]) => void;
     error?: import("svelte").Snippet<[Error]>;
     nodata?: import("svelte").Snippet;
     loading?: import("svelte").Snippet;
@@ -36,33 +36,61 @@
     >;
   }
   let {
-    paramRelays,
+    req = undefined,
     pubkey,
     localRelays,
-    req = undefined,
+    paramRelays = undefined,
+    // relayChange,
     error,
     loading,
     nodata,
     contents,
   }: Props = $props();
 
+  let result:
+    | ReqResult<DefaultRelayConfig[] | string[]>
+    | {
+        data: Readable<DefaultRelayConfig[] | string[]>;
+        status: Readable<ReqStatus>;
+        error: Readable<any>;
+      }
+    | undefined;
   let relays: DefaultRelayConfig[] | undefined = undefined;
 
   console.log(pubkey);
-  let result: any;
-  loginUser.subscribe(() => {
-    result = pubkey
-      ? deriveResult(localRelays, pubkey, req)
-      : $loginUser !== ""
-        ? deriveResult(localRelays, $loginUser, req)
-        : setLoadRelays();
+  loginUser.subscribe((value) => {
+    userChange(value);
   });
+  // $effect(() => {
+  //   userChange($loginUser);
+  //   // let result = pubkey
+  //   //   ? deriveResult(localRelays, pubkey, req)
+  //   //   : $loginUser !== ""
+  //   //     ? deriveResult(localRelays, $loginUser, req)
+  //   //     : setLoadRelays();
+  // });
+
+  function userChange(user: string) {
+    if (pubkey) {
+      result = deriveResult(localRelays, pubkey, req);
+    } else if (user && user !== "") {
+      result = deriveResult(localRelays, user, req);
+    } else {
+      console.log(user, pubkey);
+      result = setLoadRelays();
+    }
+  }
   //個々に来た段階でpubkeyがないってことは設定がないで、homeに来てたら設定に飛ぶから、それ以外のnoteのページとかに直できたときだから、
   //paramにリレーが設定されてたらそれを使ってなかったらなんか適当にデフォルトリレーセットしよう
-  function setLoadRelays() {
+  function setLoadRelays():
+    | {
+        data: Readable<DefaultRelayConfig[] | string[]>;
+        status: Readable<ReqStatus>;
+        error: Readable<any>;
+      }
+    | undefined {
     if (paramRelays) {
       setRelays(paramRelays);
-      //適当にデータ返しておこう
       return {
         data: readable(paramRelays),
         status: readable("success" as ReqStatus),
@@ -70,7 +98,6 @@
       };
     } else {
       setRelays(defaultRelays);
-      //適当にデータ返しておこう
       return {
         data: readable(defaultRelays),
         status: readable("success" as ReqStatus),
@@ -109,13 +136,36 @@
     }
   }
 
-  let data = $derived(result?.data);
+  let data:
+    | Readable<DefaultRelayConfig[] | null | undefined | string[]>
+    | string[]
+    | undefined = result?.data;
   let status = $derived(result?.status);
   let errorData = $derived(result?.error);
-  // $: console.log($status);
-  // data?.subscribe(()=>{ if (
+  // console.log($status);
+  $effect(() => {
+    if ($data && $status) {
+      dataChange($status, $data);
+    }
+  });
+  function dataChange(
+    status: string | undefined,
+    data: string | any[] | null | undefined
+  ) {
+    if ((status === "success" && !data) || (data && data.length <= 0)) {
+      //ノーデータだったときにデフォルトリレーをセット
+      setRelays(defaultRelays);
+      //適当にデータ返しておこう
+      result = {
+        data: readable(defaultRelays),
+        status: readable("success" as ReqStatus),
+        error: readable(undefined),
+      };
+    }
+  }
+  // $: if (
   //   ($status === "success" && !$data) ||
-  //   ($data !== undefined && ($data?.length??0 <= 0))
+  //   ($data !== undefined && $data.length <= 0)
   // ) {
   //   //ノーデータだったときにデフォルトリレーをセット
   //   setRelays(defaultRelays);
@@ -125,13 +175,11 @@
   //     status: readable("success" as ReqStatus),
   //     error: readable(undefined),
   //   };
-  // }})
+  // }
 </script>
 
-{#if relays}
-  {@render contents?.({ relays: relays, status: "success" })}
-{:else if $status === "success" && !$data}
-  {@render contents?.({ relays: defaultRelays, status: $status ?? "error" })}
+{#if relays}{@render contents?.({ relays: relays, status: "success" })}
+  <!-- <slot {relays} status="success" /> -->
 {:else if $errorData}
   {@render error?.($errorData)}
 {:else if $data && $data.length > 0}
@@ -144,4 +192,6 @@
   {@render loading?.()}
 {:else}
   {@render nodata?.()}
+  <!-- {@const relays = setRelays(defaultRelays)}
+  {@render children?.({ relays: defaultRelays, status: "success" })} -->
 {/if}
