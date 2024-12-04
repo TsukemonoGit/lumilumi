4ec993b3d086b1725867a1ca62e7edc3e28c3e18
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-12-04 11:23:15 +0900

    fix relay hint bug

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,6 +4,7 @@ import "$lib/i18n"; // Import to initialize. Important :)
 import { locale, waitLocale } from "svelte-i18n";
 import type { LayoutLoad } from "./$types";
 import { nip19 } from "nostr-tools";
+import { relayRegex2 } from "$lib/func/regex";
 
 export const load: LayoutLoad = async (
   params
@@ -29,7 +30,16 @@ export const load: LayoutLoad = async (
       // console.log("[decode]", type, data);
       if (type === "nevent") {
         const nevent = data as nip19.EventPointer;
-        return nevent;
+        const nrelays = nevent.relays?.filter((relay) =>
+          relayRegex2.test(relay)
+        );
+
+        return {
+          id: nevent.id,
+          relays: nrelays && nrelays.length > 0 ? nrelays : undefined,
+          kind: nevent.kind,
+          author: nevent.author,
+        };
       } else if (type === "note") {
         return { id: data as string };
       } else {
