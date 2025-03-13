3e03c60a2563347ff286c9d4ed32731ed5b487ba
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2025-03-13 16:03:58 +0900

    fix protocol_handlers

--- src/routes/[nostr=nostr]/+page.server.ts
+++ src/routes/[nostr=nostr]/+page.server.ts
@@ -4,6 +4,6 @@ export const load = async ({ params, setHeaders }) => {
   // URL が "nostr:" または "nostr://" で始まる場合、リダイレクトを行う
   const { nostr } = params;
 
-  const newPath = nostr.replace(/^nostr:\/?\/?/, "");
+  const newPath = nostr.replace(/^(?:web\+)?nostr:\/?\/?/, "");
   throw redirect(302, newPath); // 302リダイレクトで新しいURLに移動
 };
