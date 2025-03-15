7379d5084136c7899b7aa15e9a4e36ba2a3b8604
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2025-03-15 12:25:35 +0900

    fix embed contents

--- src/lib/components/NostrElements/content/EmbedTwitter.svelte
+++ src/lib/components/NostrElements/content/EmbedTwitter.svelte
@@ -10,10 +10,8 @@
 </script>
 
 <blockquote class="twitter-tweet">
-  <a
-    class="underline text-magnum-300 break-all hover:opacity-80"
-    data-chrome="noscrollbar noheader transparent"
-    href={url}><OgpLoad url={originalUrl} /></a
+  <a data-chrome="noscrollbar noheader transparent" href={url}
+    ><OgpLoad url={originalUrl} /></a
   ><script
     async
     src="https://platform.twitter.com/widgets.js"
