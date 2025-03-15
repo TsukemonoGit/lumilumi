5662dba36dea38ce8b75955fcf223ad838ee8251
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2025-03-15 12:05:43 +0900

    fix embed contents

---
+++ src/lib/components/NostrElements/content/EmbedTwitter.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  interface Props {
+    url: string;
+    originalUrl: string;
+    onError: () => void;
+  }
+  let { url, onError, originalUrl }: Props = $props();
+</script>
+
+<blockquote class="twitter-tweet">
+  <a
+    class="underline text-magnum-300 break-all hover:opacity-80"
+    data-chrome="noscrollbar noheader transparent"
+    href={url}>{originalUrl}</a
+  ><script
+    async
+    src="https://platform.twitter.com/widgets.js"
+    charset="utf-8"
+    onerror={onError}
+  ></script>
+</blockquote>
---
+++ src/lib/components/NostrElements/content/MediaEmbedSwitcher.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { isvalidURL, getYoutubeVideoId } from "$lib/func/ogp";
+  import { lumiSetting } from "$lib/stores/globalRunes.svelte";
+
+  import EmbedYoutube from "./EmbedYoutube.svelte";
+  import EmbedTwitter from "./EmbedTwitter.svelte";
+
+  let { url, children }: { url: string; children?: any } = $props();
+
+  let loadFailed: boolean = $state(false);
+  let hostname: string = $derived(isvalidURL(url) ? new URL(url).hostname : "");
+  // YouTubeかどうかとビデオIDを取得
+  let isYoutube: boolean = $derived(
+    hostname.includes("youtube.com") || hostname === "youtu.be"
+  );
+  let youtubeVideoId: string | null = $derived(getYoutubeVideoId(url));
+  // Twitter/X URLかどうかを判定
+  let isTwitter: boolean = $derived(
+    hostname.includes("twitter.com") ||
+      hostname.includes("x.com") ||
+      hostname === "t.co"
+  );
+
+  // let twitterPostId:boolean = $derived(getTwitterPostId(url));
+  // 表示するメディアの種類を判定
+  let showImageOrVideo: boolean = $derived(lumiSetting.get().showImg);
+  let showYoutubeEmbed: boolean = $derived(
+    showImageOrVideo && isYoutube && youtubeVideoId !== null && !loadFailed
+  );
+
+  let twitterUrl: string | null = $derived(normalizeTwitterUrl(url));
+  let showTwitterEmbed: boolean = $derived(
+    showImageOrVideo && isTwitter && twitterUrl !== null && !loadFailed
+  );
+
+  function handleOnError() {
+    loadFailed = true;
+  }
+
+  function normalizeTwitterUrl(url: string) {
+    return url.replace(
+      /^(?:https?:\/\/)?(?:x\.com|twitter\.com)\/([^\/]+)\/status\/(\d+)/i,
+      "https://twitter.com/$1/status/$2"
+    );
+  }
+</script>
+
+{#if showYoutubeEmbed}
+  <!--tube-->
+  <EmbedYoutube id={youtubeVideoId} onError={handleOnError} />
+{:else if showTwitterEmbed}
+  <!--twitter-->
+  <EmbedTwitter url={twitterUrl} originalUrl={url} onError={handleOnError} />
+{:else}
+  {@render children?.()}
+{/if}
