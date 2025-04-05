1803b076cb25e8202c9ba5aa7cd98b4b15aca4ff
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2025-04-05 13:11:29 +0900

    fix content tag

--- src/lib/components/NostrElements/content/ProxyTag.svelte
+++ src/lib/components/NostrElements/content/ProxyTag.svelte
@@ -11,6 +11,6 @@
 
 <Link
   href={proxyTag[1]}
-  className={"inline-flex  float-end flex-row  text-magnum-500/75 hover:underline hover:text-magnum-400/80 items-center text-sm font-semibold"}
+  className={"ml-1 inline-flex  float-end flex-row  text-magnum-500/75 hover:underline hover:text-magnum-400/80 items-center text-sm font-semibold"}
   ><MessageCircle size={16} />{proxyTag[2]}
 </Link>
