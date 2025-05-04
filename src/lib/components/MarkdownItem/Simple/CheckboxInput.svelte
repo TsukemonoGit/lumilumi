425b9a4f0af919d927b46fdd2ed41ed3845200df
TsukemonoGit <mine43try@outlook.com>
2025-05-04 20:48:05 +0900

    markdown checkbox

---
+++ src/lib/components/MarkdownItem/Simple/CheckboxInput.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import type { Token } from "markdown-it/index.js";
+  import SimpleContentBlock from "../SimpleContentBlock.svelte";
+  import { transformTokens } from "$lib/func/markdown";
+
+  interface Props {
+    part: Token;
+  }
+
+  let { part }: Props = $props();
+
+  // checked属性があるかどうかで判定
+  const isChecked = part.attrs?.some(([key, value]) => key === "checked");
+</script>
+
+<input
+  type="checkbox"
+  class="rounded-checkbox"
+  checked={isChecked}
+  disabled
+  id={part.attrs?.find(([key]) => key === "id")?.[1] ?? ""}
+/>
+
+<style lang="postcss">
+  input[type="checkbox"] {
+    width: 20px;
+    height: 20px;
+    vertical-align: middle;
+  }
+
+  .rounded-checkbox {
+    width: 24px !important;
+    height: 24px !important;
+
+    border: 1px solid theme("colors.magnum.600");
+    border-radius: 4px;
+    background-color: transparent;
+
+    appearance: none;
+    -webkit-appearance: none;
+    outline: none;
+    cursor: not-allowed;
+    position: relative;
+  }
+
+  /* チェックされている状態 */
+  .rounded-checkbox:checked {
+    background-color: theme("colors.magnum.600");
+    background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 16 16' fill='none' stroke='white' stroke-width='3' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4 8l3 3 5-5'/%3E%3C/svg%3E");
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+
+  /* 無効かつチェックされている状態 */
+  .rounded-checkbox:disabled:checked {
+    cursor: not-allowed;
+  }
+
+  /* 無効だが未チェックの状態 */
+  .rounded-checkbox:disabled:not(:checked) {
+    cursor: not-allowed;
+  }
+</style>
