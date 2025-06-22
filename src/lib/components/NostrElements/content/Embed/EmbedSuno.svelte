70cfcf77a5ed6cc83dccd0028ec72d6eb57d37b5
TsukemonoGit <mine43try@outlook.com>
2025-06-22 16:59:55 +0900

    suno umekomi

---
+++ src/lib/components/NostrElements/content/Embed/EmbedSuno.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let { url, onError } = $props();
+</script>
+
+<div
+  class="relative w-full max-h-64 rounded-lg overflow-hidden border border-magnum-400"
+>
+  <iframe
+    src={url}
+    frameborder="0"
+    allow="autoplay; encrypted-media; fullscreen"
+    allowfullscreen
+    loading="lazy"
+    onerror={onError}
+    class="absolute top-0 left-0 w-full h-full"
+    title="Suno Player"
+  ></iframe>
+</div>
--- src/lib/components/NostrElements/content/OgpLoad.svelte
+++ src/lib/components/NostrElements/content/OgpLoad.svelte
@@ -2,12 +2,21 @@
   import Link from "$lib/components/Elements/Link.svelte";
   import OgpCard from "$lib/components/Elements/OgpCard.svelte";
   import OGP from "$lib/components/renderSnippets/OGP.svelte";
+  import EmbedSuno from "./Embed/EmbedSuno.svelte";
   let { url } = $props();
+  let error = $state(false);
+  const onError = () => {
+    error = true;
+  };
 </script>
 
 <OGP url={url ?? ""}
   >{#snippet renderContent(contents)}
-    {#if contents.title !== "" || contents.image !== "" || contents.description !== ""}<!--OGP表示はTITLE必須にしておくと思ったけどそしたらXのOGPでてこなくなったから-->
+    {#if !error && contents.player && contents.player.includes("suno.ai")}
+      <EmbedSuno url={contents.player} {onError} />
+    {:else if !error && contents.playerStream && contents.playerStream.includes("suno.ai")}
+      <EmbedSuno url={contents.playerStream} {onError} />
+    {:else if contents.title !== "" || contents.image !== "" || contents.description !== ""}<!--OGP表示はTITLE必須にしておくと思ったけどそしたらXのOGPでてこなくなったから-->
       <OgpCard {contents} url={url ?? ""} />{:else}<Link
         props={{ "aria-label": `External Links: ${url}` }}
         className="underline text-magnum-300 break-all "
