3aba6e1e9aa4bec07661edf0b8fba719e6cbc9a0
TsukemonoGit <mine43try@outlook.com>
2025-06-22 17:07:20 +0900

    suno umekomi

--- src/lib/components/NostrElements/content/Embed/EmbedSuno.svelte
+++ src/lib/components/NostrElements/content/Embed/EmbedSuno.svelte
@@ -2,17 +2,14 @@
   let { url, onError } = $props();
 </script>
 
-<div
-  class="relative w-full max-h-64 rounded-lg overflow-hidden border border-magnum-400"
->
-  <iframe
-    src={url}
-    frameborder="0"
-    allow="autoplay; encrypted-media; fullscreen"
-    allowfullscreen
-    loading="lazy"
-    onerror={onError}
-    class="absolute top-0 left-0 w-full h-full"
-    title="Suno Player"
-  ></iframe>
-</div>
+<iframe
+  src={url}
+  class="w-full h-full overflow-y-hidden"
+  frameborder="0"
+  allow="encrypted-media; fullscreen"
+  allowfullscreen
+  loading="lazy"
+  onerror={onError}
+  title={"Embedded suno player"}
+  sandbox="allow-scripts allow-same-origin allow-presentation"
+></iframe>
