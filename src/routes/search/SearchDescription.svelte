7ba7a66ba02efb0d2be28363638a3e6127fbd699
TsukemonoGit <mine43try@outlook.com>
2025-09-15 13:35:26 +0900

    検索ワードがない場合はデフォリレーで検索

--- src/routes/search/SearchDescription.svelte
+++ src/routes/search/SearchDescription.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { t as _ } from '@konemono/svelte5-i18n';
+  import { t as _ } from "@konemono/svelte5-i18n";
   import Link from "$lib/components/Elements/Link.svelte";
 </script>
 
