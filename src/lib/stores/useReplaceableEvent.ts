c607c5218d8e428e3f43128949b3536f1225979d
TsukemonoGit <mine43try@outlook.com>
2025-12-21 11:10:50 +0900

    replaveableの受信の部分ちょっと修正

--- src/lib/stores/useReplaceableEvent.ts
+++ src/lib/stores/useReplaceableEvent.ts
@@ -7,9 +7,9 @@ import type {
   RxReqOverable,
   RxReqPipeable,
 } from "rx-nostr";
-import { latest } from "rx-nostr";
+import { latest, uniq } from "rx-nostr";
 import { pipe } from "rxjs";
-
+import { debounceTime } from "rxjs/operators";
 import type { ReqResult } from "$lib/types.js";
 import { useReq } from "$lib/func/useReq";
 import { tie } from "./stores";
@@ -30,10 +30,18 @@ export function useReplaceableEvent(
   initData?: EventPacket | EventPacket[] | undefined,
   staleTime: number = Infinity,
   initialDataUpdatedAt: number | undefined = undefined,
-  refetchInterval: number = Infinity
+  refetchInterval: number = Infinity,
+  bufferTimeMs: number = 300
 ): ReqResult<EventPacket> {
   const filters = [{ kinds: [kind], authors: [pubkey], limit: 1 }];
-  const operator = pipe(tie, latest());
+
+  const operator = pipe(
+    tie, // リレー接続状態に応じた再実行制御
+    uniq(), // 重複イベント除去(同一idは1回のみ通過)
+    debounceTime(bufferTimeMs), // 最後の受信からN ms経過後に処理開始
+    latest() // created_atが最新のイベントのみ出力
+  );
+
   return useReq(
     {
       queryKey,
@@ -43,6 +51,11 @@ export function useReplaceableEvent(
       initData,
     },
     undefined,
-    { staleTime, gcTime: staleTime, initialDataUpdatedAt, refetchInterval }
+    {
+      staleTime,
+      gcTime: staleTime,
+      initialDataUpdatedAt,
+      refetchInterval,
+    }
   ) as ReqResult<EventPacket>;
 }
