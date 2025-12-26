e77129eee4446405c3c868a7c1e234bf5572cc42
TsukemonoGit <mine43try@outlook.com>
2025-12-26 16:32:44 +0900

    protectマーク

---
+++ src/lib/components/Elements/Protected.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Shield } from "lucide-svelte";
+  let { zIndex }: { zIndex: number | undefined } = $props();
+</script>
+
+<Shield
+  size={14}
+  class="absolute top-1 left-0 text-yellow-600 fill-yellow-400"
+  style={`z-index:${zIndex || 10 + 1}`}
+/>
