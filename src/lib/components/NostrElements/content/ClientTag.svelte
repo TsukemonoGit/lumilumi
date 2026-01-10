200a3886d765dd6ecf8a753107f2b8cc4c031c34
TsukemonoGit <mine43try@outlook.com>
2026-01-10 21:35:55 +0900

    fix: クライアントタグクリック挙動をPopover表示に変更

--- src/lib/components/NostrElements/content/ClientTag.svelte
+++ src/lib/components/NostrElements/content/ClientTag.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { page } from "$app/state";
+  import Link from "$lib/components/Elements/Link.svelte";
+  import Popover from "$lib/components/Elements/Popover.svelte";
   import { parseNaddr } from "$lib/func/util";
   import * as nip19 from "nostr-tools/nip19";
+  import NaddrEvent from "../kindEvents/NaddrEvent.svelte";
 
   interface Props {
     tags: string[][];
@@ -12,21 +15,49 @@
   let { tags, isShowClientTag = true, depth }: Props = $props();
   let clientTag = $derived(tags.find((tag) => tag[0] === "client"));
 
-  const onClickClientTag = (atag: string[]) => {
-    const naddrAddress = parseNaddr(["a", ...atag]);
-    const encoded = nip19.naddrEncode(naddrAddress);
-    window.open(`https://nostrapp.link/a/${encoded}`, "_blank", "noreferrer");
-  };
+  let naddrAddress: undefined | nip19.AddressPointer = $derived.by(() => {
+    if (!clientTag || clientTag.length < 3) return;
+    return parseNaddr(["a", ...clientTag.slice(2)]);
+  });
+  let naddrUrl: undefined | string = $derived(
+    naddrAddress ? nip19.naddrEncode(naddrAddress) : undefined
+  );
+  let appLink: undefined | string = $derived(
+    naddrUrl ? `https://nostrapp.link/a/${naddrUrl}` : undefined
+  );
+  let isNotePage = $derived(
+    page.route.id === "/[note=note]" || page.route.id === "/[naddr=naddr]"
+  );
 </script>
 
-{#if clientTag && isShowClientTag && !(page.route.id === "/[note=note]" || (page.route.id === "/[naddr=naddr]" && depth === 0))}
-  {#if clientTag.length > 2}<button
-      title={"open in nostrapp.link"}
-      onclick={() => onClickClientTag(clientTag.slice(2))}
-      class={`ml-1 inline float-end text-sm font-semibold text-magnum-500/75 hover:underline hover:text-magnum-400/80 w-fit whitespace-pre-wrap break-words `}
+{#if clientTag && isShowClientTag && !(isNotePage && depth === 0)}
+  {#if clientTag.length > 2}<div
+      class={"ml-1 inline float-end text-sm font-semibold text-magnum-500/75 hover:underline hover:text-magnum-400/80 w-fit whitespace-pre-wrap break-words "}
       style="word-break: break-word;"
-      >via {clientTag[1]}
-    </button>{:else}<span
+    >
+      <Popover ariaLabel="client info">
+        via {clientTag[1]}
+
+        {#snippet popoverContent()}<div class=" w-80">
+            <NaddrEvent
+              data={parseNaddr(["", clientTag[2]])}
+              content={clientTag[1]}
+              depth={0}
+              displayMenu={false}
+              repostable={false}
+            />
+
+            <Link
+              href={appLink!}
+              className="text-magnum-400 hover:underline text-sm float-end"
+            >
+              Open in nostrapp.link
+            </Link>
+          </div>
+        {/snippet}
+      </Popover>
+    </div>
+  {:else}<span
       class={`ml-1 inline float-end text-sm font-semibold text-neutral-200/50 w-fit whitespace-pre-wrap break-words`}
       style="word-break: break-word;"
       >via {clientTag[1]}
