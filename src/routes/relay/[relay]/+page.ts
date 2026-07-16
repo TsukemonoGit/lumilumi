04fdf7988247b6838fabf7b340d6ac155c375bb0
mono <102149418+TsukemonoGit@users.noreply.github.com>
2026-07-16 00:09:03 +0000

    fix relay url validation

--- src/routes/relay/[relay]/+page.ts
+++ src/routes/relay/[relay]/+page.ts
@@ -1,12 +1,18 @@
+import { error } from "@sveltejs/kit";
+import { relayRegex2 } from "$lib/func/regex";
 import { ogDescription, ogTitle } from "$lib/stores/stores";
 import type { PageLoad, RouteParams } from "./$types";
 
 export const load: PageLoad<{
   relay: string;
 }> = ({ params }: { params: RouteParams }) => {
-  console.log(params.relay);
+  const decoded = decodeURIComponent(params.relay);
 
-  const relayURL = new URL(decodeURIComponent(params.relay));
+  if (!relayRegex2.test(decoded)) {
+    error(404, "Invalid relay URL");
+  }
+
+  const relayURL = new URL(decoded);
   ogTitle.set(`Lumilumi - ${relayURL.href} Timeline`);
   ogDescription.set(`${relayURL.href} Timeline`);
 
